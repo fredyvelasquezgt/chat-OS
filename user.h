c6e158eb4246fae65b162915fcc16e21791d0971
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-04-29 21:38:06 -0600

    Add files via upload

---
+++ user.h
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <netinet/in.h>
+
+using namespace std;
+
+enum UserStatus
+{
+  ACTIVE,
+  BUSY,
+  INACTIVE
+};
+
+class User
+{
+public:
+  User();
+  string username; // unique
+  UserStatus status;
+  time_t last_activity;
+  char ip_address[INET_ADDRSTRLEN]; // unique
+  int socket;
+  string to_string();
+  string get_status();
+  void set_status(string new_status);
+  void update_last_activity_time();
+};
