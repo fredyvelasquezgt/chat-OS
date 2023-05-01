385855abc928556480bd2d2b09d0e067f0611c04
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-05-01 00:04:02 -0600

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -45,3 +45,91 @@ void User::set_status(string new_status)
 void User::update_last_activity_time() {
 	time(&last_activity);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+
+enum UserStatus
+{
+    ACTIVE,
+    BUSY,
+    INACTIVE
+}
+
+class User
+{
+    public string username; // unique
+    public UserStatus status;
+    public DateTime last_activity;
+    public string ip_address; // unique
+    public int socket;
+
+    public User()
+    {
+        username = "";
+        socket = 0;
+        status = UserStatus.ACTIVE;
+    }
+
+    public string to_string()
+    {
+        string data;
+        data = "\t" + username + "\t" + ip_address + "\t" + User.get_status(status);
+        return data;
+    }
+
+    public static string get_status(UserStatus status)
+    {
+        if (status == UserStatus.ACTIVE)
+            return "ACTIVO";
+        else if (status == UserStatus.BUSY)
+            return "OCUPADO";
+        else
+            return "INACTIVO";
+    }
+
+    public void set_status(string new_status)
+    {
+        if (new_status == "ACTIVO")
+            status = UserStatus.ACTIVE;
+        else if (new_status == "OCUPADO")
+            status = UserStatus.BUSY;
+        else
+            status = UserStatus.INACTIVE;
+    }
+
+    public void update_last_activity_time()
+    {
+        last_activity = DateTime.Now;
+    }
+}
